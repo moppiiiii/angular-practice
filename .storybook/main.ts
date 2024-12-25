f817d3c165bec4d86ad8edec4f646fb0b57f7e50
moppiiiii <tnktmhssrc@gmail.com>
2024-12-25 16:18:56 +0900

    test: chromtic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/angular";
+import { StatsWriterPlugin } from 'webpack-stats-plugin';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -11,6 +12,24 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/angular",
     options: {},
+  },
+  webpackFinal: async (config) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins.push(
+      new StatsWriterPlugin({
+        filename: 'preview-stats.json',
+        fields: null,
+        stats: {
+          all: true,
+          assets: true,
+          chunks: true,
+          modules: true,
+        }
+      })
+    );
+
+    return config;
   },
 };
 export default config;
