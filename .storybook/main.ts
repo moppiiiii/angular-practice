9a0954071ec960851e7b6b9125ac5b26c5fa9013
moppiiiii <tnktmhssrc@gmail.com>
2024-12-25 16:02:33 +0900

    fix: command

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/angular";
+import StatsWriterPlugin from 'webpack-stats-plugin';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -11,6 +12,19 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/angular",
     options: {},
+  },
+  webpackFinal: async (config) => {
+    // plugins が未定義なら初期化
+    config.plugins = config.plugins || [];
+
+    config.plugins.push(
+      new StatsWriterPlugin({
+        filename: 'preview-stats.json', // Chromaticのデフォルト名称
+        fields: null, // すべてのstatsフィールドを出力（必要に応じて絞り込み可能）
+      })
+    );
+
+    return config;
   },
 };
 export default config;
